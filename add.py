cef43441734605e5b944e05e16a49445690b4d0f
sahuledk <sahuledk@gmail.com>
2022-10-28 15:49:14 +0530

    pushing program add.py

---
+++ add.py
@@ -0,0 +1,12 @@
+# This program adds two numbers
+
+# Store input numbers.
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+
+# Add two numbers
+result = a + b
+
+# Display the sum.
+print("The sum of {0}, {1} is {2}".format(a, b, result))
+
